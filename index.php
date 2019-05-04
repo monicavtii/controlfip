6f09dd3ee37d9dd2cfafc9729addae47dcd873e3
monica <monicavtii@gmail.com>
2019-05-03 21:33:37 -0300

    Arquivo principal do sistema

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Home | ControlFip</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>ControlFip</h1>
+		<h2>Sistema de Controle Finaneiro Pessoal</h2>
+	</body>
+</html>
