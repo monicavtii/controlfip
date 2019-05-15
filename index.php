e54f1f9413ddb3df59d6ac94b477a06b7bdcb655
Mônica Vicente <monicavtii@gmail.com>
2019-05-14 21:29:20 -0300

    Atualizaçao do index

--- index.php
+++ index.php
@@ -10,7 +10,26 @@
 		<div>
 			<a href="contas-apagar.php">Contas a pagar </a>
 			<a href="contas-areceber.php">Contas a receber </a>
-		</div>	
-
+		</div>
+		<div>
+			<table>
+				<thead>
+					<tr>
+						<th>Mês</th>
+						<th>Total previst</th>
+						<th>Total real</th>
+						<th>Saldo na carteira</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Abril</td>
+						<td>R$ 5000.00</td>
+						<td>R$ 7000.00</td>
+						<td>R$ 4200.00</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</body>
 </html>
